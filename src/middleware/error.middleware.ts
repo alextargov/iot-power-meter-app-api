26810db0eee9579876325239864647b233d79263
Alexander Targov <alexander.targov@synchronoss.com>
2020-03-14 20:25:28 +0200

    Fix middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -3,9 +3,9 @@ import { Request, Response, NextFunction } from 'express';
 
 export const errorMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => res.status(error.status).json({
     statusCode: error.status,
-    error: error.response.name || error.response.error || error.name,
-    message: error.response.message || error.response || error.message,
-    errors: error.response.errors || null,
+    error: error.name,
+    message: error.message,
+    errors: error.errors || null,
     timestamp: new Date().toISOString(),
     path: req ? req.url : null,
 });
