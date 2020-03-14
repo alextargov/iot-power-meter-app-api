6d53009f29501e638804ce3a8dd55a43c841dee5
Alexander Targov <alexander.targov@synchronoss.com>
2020-03-14 20:27:12 +0200

    Add a measurement model

---
+++ src/models/measurement.ts
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+export interface IMeasurement {
+    id?: string;
+    appliance: string;
+    current: number;
+    voltage: number;
+
+    createdAt?: Date;
+    updatedAt?: Date;
+};
+
+interface IMeasurementDocument extends IMeasurement, mongoose.Document {
+    id?: string
+};
+
+const schemaOptions = {
+    collection: 'measurements',
+    minimize: false,
+    strict: false,
+    versionKey: false,
+    timestamps: true
+};
+
+const MeasurementSchema = new mongoose.Schema({
+    appliance: {
+        type: mongoose.SchemaTypes.String,
+        required: true
+    },
+    current: {
+        type: mongoose.SchemaTypes.Number,
+        required: true
+    },
+    voltage: {
+        type: mongoose.SchemaTypes.Number,
+        required: true
+    },
+    createdAt: {
+        type: mongoose.SchemaTypes.Date,
+        required: true
+    },
+    updatedAt: {
+        type: mongoose.SchemaTypes.Date,
+        required: false
+    },
+}, schemaOptions);
+
+const Measurement = mongoose.model<IMeasurementDocument>('Measurements', MeasurementSchema);
+
+export { Measurement };
