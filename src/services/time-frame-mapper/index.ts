1783e5de8599e275cab62fea49017bda8e2f49d6
Alexander Targov <alexander.targov@synchronoss.com>
2020-03-29 12:41:31 +0300

    Enhance time frame mapper

--- src/services/time-frame-mapper/index.ts
+++ src/services/time-frame-mapper/index.ts
@@ -3,30 +3,60 @@ import moment from 'moment';
 import { loggerService } from '../logger';
 import { Config } from '../../models/config';
 import { TimeFrames } from './time-frames.enum';
+import { ITimeFrame } from './time-frame.interface';
 
 const logNamespace = 'TimeFrameMapperService';
 
-const mapTimeFrames = async (): Promise<Date> => {
+const mapTimeFrames = async (routeTimeFrame: ITimeFrame): Promise<ITimeFrame> => {
     loggerService.debug(`[${logNamespace}]: mapTimeFrames(): Get time frames.`);
 
     const result = await Config.findOne({ key: 'timeFrames' }).exec();
 
-    return result.value.map((config) => {
-        switch (config) {
+    return result.value.reduce((collection, config) => {
+        switch (config.frame) {
             case TimeFrames.last30days:
-                // tslint:disable-next-line: no-magic-numbers
-                return moment.utc().subtract(30, 'days').toDate();
+                return {
+                    ...collection,
+                    [config.frame]: {
+                        frame: config.frame,
+                        // tslint:disable-next-line: no-magic-numbers
+                        startDate: moment().subtract(30, 'days').toDate(),
+                        endDate: moment().toDate(),
+                    },
+                };
             case TimeFrames.last7days:
-                // tslint:disable-next-line: no-magic-numbers
-                return moment.utc().subtract(7, 'days').toDate();
+                return {
+                    ...collection,
+                    [config.frame]: {
+                        frame: config.frame,
+                        // tslint:disable-next-line: no-magic-numbers
+                        startDate: moment().subtract(7, 'days').toDate(),
+                        endDate: moment().toDate(),
+                    },
+                };
             case TimeFrames.today:
-                return moment.utc().startOf('day');
+                return {
+                    ...collection,
+                    [config.frame]: {
+                        frame: config.frame,
+                        startDate: moment().startOf('day').toDate(),
+                        endDate: moment().toDate(),
+                    },
+                };
             case TimeFrames.custom:
-            default: return undefined;
+            default:
+                return {
+                    ...collection,
+                    [config.frame]: {
+                        frame: config.frame,
+                        startDate: moment(routeTimeFrame.startDate).startOf('day').toDate(),
+                        endDate: moment(routeTimeFrame.endDate).endOf('day').toDate(),
+                    },
+                };
         }
-    });
+    }, {});
 };
 
-export const measurementService = {
+export const timeFrameMapperService = {
     mapTimeFrames,
 };
