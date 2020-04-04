605e57ed2d783dd932330c8de070a55d3836a6bd
Alexander Targov <alexander.targov@synchronoss.com>
2020-04-04 22:38:06 +0300

    Add time frame route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,16 @@ import { Router } from 'express';
 
 import { controller as measurementController } from './measurement.controller';
 import { controller as configController } from './config.controller';
+import { controller as timeFrameController } from './time-frame.controller';
+
 import { timeFrameMiddleware } from '../middleware/time-frame.middleware';
 
 const router = Router();
+
 router.use(timeFrameMiddleware);
+
 router.use('/measurement', measurementController);
 router.use('/config', configController);
+router.use('/timeFrame', timeFrameController);
 
 export const routes = router;
---
+++ src/routes/time-frame.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response, Router } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+
+import { loggerService } from '../services/logger';
+import { timeFrameMapperService } from '../services/time-frame-mapper';
+import { ITimeFrame } from '../services/time-frame-mapper/time-frame.interface';
+
+const logNamespace = 'TimeFrameController';
+const router = Router();
+
+const getTimeFrame = async (req: Request, res: Response) => {
+    const { frame, startDate, endDate } = req.query;
+
+    try {
+        const timeFrames: ITimeFrame = {
+            frame,
+            startDate,
+            endDate,
+        };
+
+        const result = await timeFrameMapperService.mapTimeFrames(timeFrames);
+
+        return res.json(result);
+    } catch (error) {
+        loggerService.error(`[${logNamespace}]: Could not list time frames due to error: ${error}`);
+
+        throw new Error('Unable to list time frames');
+    }
+};
+
+router.get('/', expressAsyncHandler(getTimeFrame));
+
+export const controller = router;
